601f8286bbc198682d3c85a2464bbd3728f98f51
Mauricio Peccorini <mauricio.peccorini@openbravo.com>
2019-12-24 13:29:11 +0100

    Adapt OSP Cache to refactored Cache Manager

--- src/org/openbravo/cache/corecaches/OSPCacheManager.java
+++ src/org/openbravo/cache/corecaches/OSPCacheManager.java
@@ -2,17 +2,15 @@ package org.openbravo.cache.corecaches;
 
 import java.util.Map;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.openbravo.cache.CacheManager;
+import org.openbravo.cache.impl.ConcurrentMapCacheManager;
 import org.openbravo.dal.security.OrganizationStructureProvider;
 
+@ApplicationScoped
+@CacheManager.Qualifier("OrganizationStructure")
 public class OSPCacheManager
-    extends CacheManager<String, Map<String, OrganizationStructureProvider.OrgNode>> {
-
-  private static final String CACHE_IDENTIFIER = "OrganizationStructure";
-
-  @Override
-  public String getCacheIdentifier() {
-    return CACHE_IDENTIFIER;
-  }
+    extends ConcurrentMapCacheManager<String, Map<String, OrganizationStructureProvider.OrgNode>> {
 
 }
